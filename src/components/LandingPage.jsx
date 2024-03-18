3d0ac20a9a84b62025129a5c40edf89eeac29efe
prakharkhare999 <prakhark113@gmail.com>
2024-03-18 15:28:53 +0530

    changes

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const LandingPage = () => {
+  return (
+    <div className='w-full h-screen bg-zinc-900'>
+
+<div className='textstructure'>
+    
+</div>
+
+
+
+
+
+    </div>
+  )
+}
+export default Landing
